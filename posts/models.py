aca52320fcd70bc60da6a37772cce93c462f302a
Joel Bebo <joelbebo@gmail.com>
2019-06-25 12:19:43 -0400

    loop through posts to show on index page

--- posts/models.py
+++ posts/models.py
@@ -6,5 +6,11 @@ class Posts(models.Model):
     title = models.CharField(max_length=200)
     body = models. TextField()
     created_at = models.DateTimeField(default=datetime.now, blank=True)
+
+    # re-name post objects in admin area to output title of post"
+    def __str__(self):
+        return self.title
+
+    # correct double ss in Postss in admin area:
     class Meta:
         verbose_name_plural = 'Posts'
--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Posts
 
 def index(request):
-    return render(request, 'posts/index.html', {
-    'title': 'Latest Posts'
 
-    })
+    posts = Posts.objects.all()[:10]
+
+    context = {
+        'title': 'Latest Posts',
+        'posts': posts
+
+    }
+
+
+    return render(request, 'posts/index.html', context)
+
+    # return render(request, 'posts/index.html', {
+    # 'title': 'Latest Posts'
+    # })
